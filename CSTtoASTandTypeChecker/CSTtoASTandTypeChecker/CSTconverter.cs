a6615c62f303468b939e9a1869e1467b094f9fb8
NiklasBH <112945809+NiklasBH@users.noreply.github.com>
2024-03-28 13:13:00 +0100

    .

--- CSTtoASTandTypeChecker/CSTtoASTandTypeChecker/CSTconverter.cs
+++ CSTtoASTandTypeChecker/CSTtoASTandTypeChecker/CSTconverter.cs
@@ -2,11 +2,21 @@
 
 internal class CSTconverter : SyntaxBaseVisitor<Node>
 {
+    /// <summary>
+    /// Program Visit (visit all children)
+    /// </summary>
+    /// <param name="context"></param>
+    /// <returns></returns>
     public override Node VisitProgram(SyntaxParser.ProgramContext context)
     {
         return Visit(context.commands());
     }
 
+    /// <summary>
+    /// Assignment Visits
+    /// </summary>
+    /// <param name="context"></param>
+    /// <returns></returns>
     public override Node VisitAssignStatement(SyntaxParser.AssignStatementContext context)
     {
         return VisitAssignment(context.@this);
@@ -20,6 +30,11 @@ internal class CSTconverter : SyntaxBaseVisitor<Node>
         return node;
     }
 
+    /// <summary>
+    /// Expression Visits
+    /// </summary>
+    /// <param name="context"></param>
+    /// <returns></returns>
     public override Node VisitInfixExpression(SyntaxParser.InfixExpressionContext context)
     {
         return base.VisitInfixExpression(context);
@@ -45,6 +60,31 @@ internal class CSTconverter : SyntaxBaseVisitor<Node>
         return Visit(context.@this);
     }
 
+    /// <summary>
+    /// All Value Visits
+    /// </summary>
+    /// <param name="context"></param>
+    /// <returns></returns>
+    public override Node VisitUseValue(SyntaxParser.UseValueContext context)
+    {
+        return new TextNode(context.@this.GetText());
+    }
+
+    public override Node VisitReadValue(SyntaxParser.ReadValueContext context)
+    {
+        return new TextNode(context.@this.GetText());
+    }
+
+    public override Node VisitFlagValue(SyntaxParser.FlagValueContext context)
+    {
+        return new TextNode(context.@this.GetText());
+    }
+
+    public override Node VisitLengthOfValue(SyntaxParser.LengthOfValueContext context)
+    {
+        return new TextNode(context.@this.GetText());
+    }
+
     public override Node VisitNumberValue(SyntaxParser.NumberValueContext context)
     {
         return new NumberNode(double.Parse(context.@this.GetText()));
@@ -54,4 +94,114 @@ internal class CSTconverter : SyntaxBaseVisitor<Node>
     {
         return new TextNode(context.@this.GetText());
     }
+
+    public override Node VisitListElementValue(SyntaxParser.ListElementValueContext context)
+    {
+        return new TextNode(context.@this.GetText());
+    }
+
+    public override Node VisitIdentifierValue(SyntaxParser.IdentifierValueContext context)
+    {
+        return new TextNode(context.@id.GetText());
+    }
+
+/// <summary>
+/// Command visits
+/// </summary>
+/// <param name="context"></param>
+/// <returns></returns>
+    public override Node VisitNotLastCommand(SyntaxParser.NotLastCommandContext context)
+    {
+        Visit(context.@this);
+        return Visit(context.@next);
+    }
+
+    public override Node VisitLastCommand(SyntaxParser.LastCommandContext context)
+    {
+        return Visit(context.@this);
+    }
+
+    public override Node VisitFunctionCommand(SyntaxParser.FunctionCommandContext context)
+    {
+        return Visit(context.@this);
+    }
+
+    public override Node VisitTermsCommand(SyntaxParser.TermsCommandContext context)
+    {
+        return Visit(context.@this);
+    }
+
+    /// <summary>
+    /// Terms visits
+    /// </summary>
+    /// <param name="context"></param>
+    /// <returns></returns>
+    public override Node VisitNotLastTerm(SyntaxParser.NotLastTermContext context)
+    {
+        return Visit(context.@this);
+    }
+
+    public override Node VisitLastTerm(SyntaxParser.LastTermContext context)
+    {
+        return Visit(context.@this);
+    }
+
+    public override Node VisitStatementTerm(SyntaxParser.StatementTermContext context)
+    {
+        return Visit(context.@this);
+    }
+
+    public override Node VisitControlTerm(SyntaxParser.ControlTermContext context)
+    {
+        return Visit(context.@this);
+    }
+
+    public override Node VisitCommentTerm(SyntaxParser.CommentTermContext context)
+    {
+        return Visit(context.@this);
+    }
+
+    /// <summary>
+    /// Length of visit
+    /// </summary>
+    /// <param name="context"></param>
+    /// <returns></returns>
+    public override Node VisitLengthOf(SyntaxParser.LengthOfContext context)
+    {
+        return Visit(context.@id);
+    }
+
+    /// <summary>
+    /// Type visit
+    /// </summary>
+    /// <param name="context"></param>
+    /// <returns></returns>
+    public override Node VisitType(SyntaxParser.TypeContext context)
+    {
+        return Visit(context.@tp);
+    }
+
+    /// <summary>
+    /// Identifier visit
+    /// </summary>
+    /// <param name="context"></param>
+    /// <returns></returns>
+    public override Node VisitIdentifier(SyntaxParser.IdentifierContext context)
+    {
+        return Visit(context.@id);
+    }
+
+    /// <summary>
+    /// ListElement visit
+    /// </summary>
+    /// <param name="context"></param>
+    /// <returns></returns>
+    public override Node VisitListElement(SyntaxParser.ListElementContext context)
+    {
+        return Visit(context.@id);
+    }
+
+
+
+
 }
