800e4057e81c48e5621323f877560f3e293b925b
mathiaszip <mathiasml@hotmail.dk>
2024-05-01 13:19:42 +0200

    Update UnitTester.cs

--- CSTtoASTandTypeChecker/TranspilerTester/UnitTester.cs
+++ CSTtoASTandTypeChecker/TranspilerTester/UnitTester.cs
@@ -1,11 +1,53 @@
-﻿namespace TranspilerTester;
+﻿using System;
+using CSTtoASTandTypeChecker;
 
-public class UnitTester
+namespace TranspilerTester
 {
-    public static void testAll()
+    public class UnitTester
     {
-        //call all testmethods in the class
+        public static void TestAll()
+        {
+            TestAdditionNodeTypeCheck_Success();
+            TestAdditionNodeTypeCheck_Failure();
+        }
+
+        public static void TestAdditionNodeTypeCheck_Success()
+        {
+            // Arrange
+            AdditionNode additionNode = new AdditionNode();
+            additionNode.left = new NumberNode(5);
+            additionNode.right = new NumberNode(10);
+
+            // Act
+            TypeNode result = additionNode.typeCheck();
+
+            // Assert
+            if (!(result is NumberTypeNode))
+            {
+                throw new Exception("TestAdditionNodeTypeCheck_Success failed");
+            }
+            
+            Console.WriteLine("TestAdditionNodeTypeCheck_Success passed");
+        }
+
+        public static void TestAdditionNodeTypeCheck_Failure()
+        {
+            // Arrange
+            AdditionNode additionNode = new AdditionNode();
+            additionNode.left = new NumberNode(5);
+            additionNode.right = new TextNode("10");
+
+            // Act & Assert
+            try
+            {
+                additionNode.typeCheck();
+                throw new Exception("TestAdditionNodeTypeCheck_Failure did not throw exception as expected");
+            }
+            catch (Exception)
+            {
+                // Test passed
+                Console.WriteLine("TestAdditionNodeTypeCheck_Failure passed");
+            }
+        }
     }
-    
-    //UnitTest methods
 }
