a2e25f99bbf545e55c1428a48cd7cc8d2717498a
mathiaszip <mathiasml@hotmail.dk>
2024-05-02 13:37:08 +0200

    Unit testing complete

--- CSTtoASTandTypeChecker/CSTtoASTandTypeCheckerUnitTesting/UnitTest1.cs
+++ CSTtoASTandTypeChecker/CSTtoASTandTypeCheckerUnitTesting/UnitTest1.cs
@@ -1,10 +1,12 @@
 using System;
 using Xunit;
+using Antlr4.Runtime;
 using CSTtoASTandTypeChecker;
+
 
 namespace CSTtoASTandTypeCheckerUnitTesting
 {
-    public class TypeChecking
+    public class TypeCheckingTests
     {
         [Fact]
         public void TestAdditionNodeTypeCheck_Success()
@@ -35,12 +37,43 @@ namespace CSTtoASTandTypeCheckerUnitTesting
         }
     }
 
-    public class CSTConverter
+    public class CodeGenerationTests
     {
         [Fact]
-        public void next_test()
+        public void TestPrintCodegeneration_Success() 
         {
+            //Arrange
+            CodeGeneratorVisitor cgv = new CodeGeneratorVisitor();
+            TextNode TextNode = new TextNode("\"Hello\"");
+            PrintNode PrintNode = new PrintNode(TextNode);
+            var expected = "public static void main(String[] args){\nSystem.out.println(\"Hello\");";
+
+            //Act
+            PrintNode.generate(cgv);
             
+            //Assert
+            Assert.Equal(expected, cgv.output);
+        }
+
+        [Fact]
+        public void TestCreateNumberVariable_Success()
+        {
+            //Arrange
+            CodeGeneratorVisitor cgv = new CodeGeneratorVisitor();
+            IdentifierNode name = new IdentifierNode();
+            name.name = "variable";
+            name.type = new NumberTypeNode();
+            NumberNode value = new NumberNode(10);
+         
+            CreateVariableNode createVariableNode = new CreateVariableNode(name, new NumberTypeNode(), value);
+
+            var expected = "public static void main(String[] args){\npublic static Float variable = 10F;";
+
+            //Act
+            createVariableNode.generate(cgv);
+
+            //Assert
+            Assert.Equal(expected, cgv.output);
         }
     }
 }
